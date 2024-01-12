66ebc2c7114ef942701377fbf27040140f9b12ad
MMK <staph103@gmail.com>
2024-01-12 12:01:48 -0500

    worked on get calls for index controls

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const { User } = require("../models");
+
+router.get("/", async (req, res) => {
+  try {
+    res.render("homepage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/login", async (req, res) => {
+    try {
+      res.render("login");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get("/signup", async (req, res) => {
+    try {
+      res.render("signup");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+module.exports = router;
