d780a820b2a1b32c78f0e6d2cd9884cbb9720572
MMK <staph103@gmail.com>
2024-01-13 13:50:28 -0500

    messed up my branch

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const { User, Tour, TourComment, Memos, MemosComment, TourCategory, TourMembers, Category } = require('../models');
+const router = require("express").Router();
+const { User } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
@@ -28,41 +28,4 @@ router.get("/login", async (req, res) => {
     }
   });
   
-
-router.get('/', async (req, res) => {
-    res.render('homepage');
-});
-
-
-// router.get('/dashboard', async (req, res) => {
-//     res.render('dashboard');
-// })
-
-
-router.get('/dashboard/', async (req, res) => {
-    try {
-        req.session.user_id = 1;
-        const userData = await User.findByPk(req.session.user_id, {
-            attributes: { exclude: ['password'] },
-            include: [{ model: Tour, include: { model: User, through: { attributes: [] } }, through: { attributes: [] } }]
-        });
-
-        const user = userData.get({ plain: true });
-        console.log(user);
-        console.log(user.tours[0].users);
-
-        req.session.save(() => {
-            req.session.user_id = 1;
-            req.session.logged_in = true;
-
-            res.render('dashboard', {
-                user,
-                logged_in: req.session.logged_in
-            })
-        })
-    } catch (error) {
-        res.status(500).json(error);
-    }
-})
-
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -27,12 +27,12 @@ const signupFormHandler = async (event) => {
 
   document.location.replace("/signup");
 
-  const name = document.querySelector("#name-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const name = document.querySelector("#name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
 
   if (name && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/users", {
       method: "POST",
       body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
@@ -52,4 +52,4 @@ document
 
 document
   .querySelector(".signup-btn")
-  .addEventListener("click", signupFormHandler);
+  .addEventListener("submit", signupFormHandler);
