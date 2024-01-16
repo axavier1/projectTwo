6667ff0c825a58b45e4022ed99bc2d9b7dbc052d
Mykhailo <sharkon90@gmail.com>
2024-01-15 20:12:44 -0500

    fixing naw bar and user icon

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,32 +1,34 @@
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-    console.log('working')
-    document.location.replace("/signup");
-  
-    const user_name = document.querySelector("#name").value.trim();
-    const email = document.querySelector("#email").value.trim();
-    const password = document.querySelector("#password").value.trim();
-  
-    if (user_name && email && password) {
-      const response = await fetch("/api/user/signup", {
-        method: "POST",
-        body: JSON.stringify({ user_name, email, password }),
-        headers: { 'Content-Type': 'application/json'}
+  event.preventDefault();
+  console.log('working')
+  // document.location.replace("/signup");
 
-        
-      });
-  
-      if (response.ok) {
-        console.log('new account')
-        document.location.replace("/dashboard");
+  const user_name = document.querySelector("#name").value.trim();
+  const email = document.querySelector("#email").value.trim();
+  const password = document.querySelector("#password").value.trim();
+  const confPassword = document.querySelector("#confPassword").value.trim();
+  if (password != confPassword) {
+    alert(`password not match`)
+  }
+  if (user_name && email && password) {
+    const response = await fetch("/api/users/signup", {
+      method: "POST",
+      body: JSON.stringify({ user_name, email, password }),
+      headers: { 'Content-Type': 'application/json' }
 
-      } else {
-        alert(response.statusText);
-      }
+
+    });
+
+    if (response.ok) {
+      console.log('new account')
+      document.location.replace("/dashboard");
+
+    } else {
+      alert(response.statusText);
     }
-  };
-  
-  document
-    .querySelector(".signup-btn")
-    .addEventListener("click", signupFormHandler);
-  
+  }
+};
+
+document
+  .querySelector("#signup-btn")
+  .addEventListener("click", signupFormHandler);
