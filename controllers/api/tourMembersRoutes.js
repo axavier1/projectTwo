3d9aef764e73ff75db2d81a27c93441a14892106
Mykhailo <sharkon90@gmail.com>
2024-01-17 15:25:50 -0500

    canceling tour

--- controllers/api/tourMembersRoutes.js
+++ controllers/api/tourMembersRoutes.js
@@ -17,4 +17,24 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.delete('/:id', isLogged, async (req, res) => {
+    try {
+        const memberData = await TourMembers.destroy({
+            where: {
+                user_id: req.session.user_id,
+                tour_id: req.params.id
+            },
+        });
+
+        if (!memberData) {
+            res.status(404).json({ message: 'No data found with this id!' });
+            return;
+        }
+
+        res.status(200).json(memberData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 module.exports = router;
